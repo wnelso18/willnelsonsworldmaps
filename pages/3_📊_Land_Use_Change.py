f8b8da49e28666e179ecfa0756a5c7d0ee5580b7
Will Nelson <wnelso18@vols.utk.edu>
2023-03-21 01:10:21 -0400

    scatter plot in land use changes

--- pages/3_📊_Land_Use_Change.py
+++ pages/3_📊_Land_Use_Change.py
@@ -9,6 +9,7 @@ import uuid
 import fiona
 import sankee
 import plotly.graph_objects as go
+import plotly.express as px
 
 
 
@@ -89,12 +90,13 @@ with row1_col2:
         st.write("Select which diagram(s) you would like to view below:")
         histogram = st.checkbox("Histogram")
         pie_chart = st.checkbox("Pie Chart")
+        scatter_plot = st.checkbox("Scatter Plot")
         st.write("Note: Year selected above file uploader will be used for the histogram and pie chart")
         # st.markdown("""--------------""")
         # sankey = st.checkbox("Sankey Diagram")
-        # st.write("Select what years you would like to compare for the sankey diagram:")
-        # year1 = st.selectbox('Year 1', ('2001', '2004', '2006', '2008', '2011', '2013', '2016', '2019'))
-        # year2 = st.selectbox('Year 2', ('2001', '2004', '2006', '2008', '2011', '2013', '2016', '2019'))
+        st.write("Select what years you would like to compare for the scatter plot:")
+        year1 = st.selectbox('Year 1', ('2001', '2004', '2006', '2008', '2011', '2013', '2016', '2019'))
+        year2 = st.selectbox('Year 2', ('2001', '2004', '2006', '2008', '2011', '2013', '2016', '2019'))
 
         submit_button = st.form_submit_button("Submit")
     
@@ -102,6 +104,7 @@ with row1_col2:
         st.write(f"You selected:") 
         st.write(f"Histogram = *{histogram}*")
         st.write(f"Pie Chart = *{pie_chart}*")
+        st.write(f"Scatter Plot Comparison = *{scatter_plot}*")
         # st.write(f"Sankey Diagram = *{sankey}*")
         # st.write(f"Start Year: {year1}, \t End Year: {year2}")
 
@@ -212,8 +215,7 @@ with row1_col2:
 #                 with row1_col1:
 #                     # Display the Sankey diagram in Streamlit
 #                     st.plotly_chart(fig, use_container_width=True)
-
-
+        
         # HISTOGRAM -------------------------------------------------
 
         if histogram == True:
@@ -273,3 +275,61 @@ with row1_col2:
             )
             with row1_col1:
                 st.plotly_chart(chart2, use_container_width=True)
+        
+        # SCATTER PLOT ----------------------------------------------
+
+        if scatter_plot == True:
+
+            nlcd2 = dataset.filter(ee.Filter.eq('system:index', f'{year1}')).first()
+            landcover2 = nlcd2.select('landcover')
+        
+            nlcd3 = dataset.filter(ee.Filter.eq('system:index', f'{year2}')).first()
+            landcover3 = nlcd3.select('landcover')
+
+
+            year1_csv = "zonal_stats_year1.csv"
+            year2_csv = "zonal_stats_year2.csv"
+
+            geemap.zonal_stats_by_group(landcover2, st.session_state["roi"], year1_csv, statistics_type='SUM', denominator=1000000, scale=1000)
+            geemap.zonal_stats_by_group(landcover3, st.session_state["roi"], year2_csv, statistics_type='SUM', denominator=1000000, scale=1000)
+            
+            
+            # TRANSPOSE THE CSV FILES --------------------------------------------
+
+            gdf_csv = gpd.read_file(year1_csv)
+            transposed_gdf = gdf_csv.T
+            zst1 = transposed_gdf.to_csv('year1_transposed.csv', index=True, header=True)
+            zst1 = pd.read_csv('year1_transposed.csv')
+            zst1 = zst1.set_index('Unnamed: 0') # set the index to the first column
+            zst1 = zst1.drop('Class_sum', axis=0) # drop the row that contains 'Class_sum'
+            zst1 = zst1.drop('system:index', axis=0) # drop the row that contains 'system:index'
+            zst1.to_csv('year1_transposed.csv', index=True, header=True)
+            zst1 = pd.read_csv('year1_transposed.csv')
+            
+            gdf_csv = gpd.read_file(year2_csv)
+            transposed_gdf = gdf_csv.T
+            zst2 = transposed_gdf.to_csv('year2_transposed.csv', index=True, header=True)
+            zst2 = pd.read_csv('year2_transposed.csv')
+            zst2 = zst2.set_index('Unnamed: 0') # set the index to the first column
+            zst2 = zst2.drop('Class_sum', axis=0) # drop the row that contains 'Class_sum'
+            zst2 = zst2.drop('system:index', axis=0) # drop the row that contains 'system:index'
+            zst2.to_csv('year2_transposed.csv', index=True, header=True)
+            zst2 = pd.read_csv('year2_transposed.csv')
+
+            merged_df1 = pd.merge(zst1, zst2, on='Unnamed: 0')
+
+            # Reshape the dataframe
+            merged_df = merged_df1.melt(id_vars=['Unnamed: 0'], var_name='File', value_name='Coverage (km2)')
+
+            # Create a scatter plot to compare the two files
+            fig = px.scatter(merged_df, x='Unnamed: 0', y='Coverage (km2)', color='File', 
+                            labels={"x": "Landcover", "y": "Coverage (km2)"}, 
+                            title="Comparison of Year 1 (0_x) and Year 2 (0_y)", 
+                            hover_data=['File', 'Coverage (km2)'])
+
+            # Update the y-axis labels to indicate which file they belong to
+            fig.update_yaxes(title_text="Coverage (km2) - File 1", tickprefix="File 1: ")
+            fig.update_yaxes(title_text="Coverage (km2) - File 2", tickprefix="File 2: ", side="right")
+
+            with row1_col1:
+                st.plotly_chart(fig, use_container_width=True)
