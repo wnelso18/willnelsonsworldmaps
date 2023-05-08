8e226a847f495516ec26970c04407a8cf020fe93
Will Nelson <wnelso18@vols.utk.edu>
2023-05-08 14:14:23 -0400

    f

---
+++ pages/4_🗺️_Basemaps.py
@@ -0,0 +1,30 @@
+
+import streamlit as st
+import leafmap.foliumap as leafmap
+
+st.set_page_config(layout="wide")
+
+st.sidebar.info('Credits:')
+st.sidebar.markdown('leafmap foliumap module')
+
+col1, col2 = st.columns([7, 3])
+
+options = list(leafmap.basemaps.keys())
+
+
+with col2:
+    dropdown = st.selectbox("Basemap", options)
+
+    default_url = leafmap.basemaps[dropdown].tiles
+
+    url = st.text_input("Enter URL", default_url)
+
+m = leafmap.Map()
+m.add_basemap(dropdown)
+
+if url:
+    m.add_tile_layer(url, name='Tile Layer', attribution=' ')
+
+with col1:
+    m.to_streamlit()
+
