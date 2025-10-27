48bd67384b97c77610f3d2e7644ce968c35e60a3
Will Nelson <wnelso18@vols.utk.edu>
2025-10-26 21:29:25 -0400

    basemaps page

--- pages/4_🗺️_Basemaps.py
+++ pages/4_🗺️_Basemaps.py
@@ -1,6 +1,7 @@
 
 import streamlit as st
 import leafmap.foliumap as leafmap
+import ee
 
 # AUTHENTICATE AND INITIALIZE EARTH ENGINE-----------------------------------------------------------------------
 import json, os
