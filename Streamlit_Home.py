d3c2cb4f6ce66fec1bc746a21dc2d00141230559
wnelso18 <89172680+wnelso18@users.noreply.github.com>
2026-01-05 09:34:46 -0500

    Update date filter for MODIS dataset

--- Streamlit_Home.py
+++ Streamlit_Home.py
@@ -84,7 +84,7 @@ MapS = geemap.Map()
 collection = ee.FeatureCollection("TIGER/2018/States")
 
 dataset = ee.ImageCollection('MODIS/061/MOD10A1') \
-          .filter(ee.Filter.date('2025-11-20', '2025-12-02'))
+          .filter(ee.Filter.date('2025-12-20', '2026-1-05'))
 
 country = (ee.FeatureCollection('users/giswqs/public/countries'))
 
